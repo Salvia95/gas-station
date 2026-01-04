c4d833590ac31a2ddf5de55cd378e1015afd6f28
Salvia95 <llls2542@gmail.com>
2026-01-04 14:27:57 +0900

    Events 패키지 추가, NamedInterface 설정

---
+++ src/main/kotlin/io/salvia/gas_station/station/events/ModuleMetadata.kt
@@ -0,0 +1,8 @@
+package io.salvia.gas_station.station.events
+
+import org.springframework.modulith.NamedInterface
+import org.springframework.modulith.PackageInfo
+
+@NamedInterface
+@PackageInfo
+internal class ModuleMetadata
